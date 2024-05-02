4141a1068aa42337550aeeac63eb8c3fdeb4ccc4
gorilla-san <108207000+gorilla-san@users.noreply.github.com>
2024-05-02 08:30:12 +0200

    asd

--- slide-in.js
+++ slide-in.js
@@ -1,19 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
   const urlParams = new URLSearchParams(window.location.search);
   const slideIn = urlParams.get('slide-in');
 
   if (slideIn === 'true') {
-    let sections = document.querySelectorAll('.sections');
-    sections[0].style.transform = 'translateY(100dvh)';
-    sections[1].style.transform = 'translateY(100dvh)';
-    sections[0].style.transition = 'all 0.5s';
-    sections[1].style.transition = 'all 0.5s';
-    setTimeout(()=>{
-      sections[0].style.transform = 'translateY(-100dvh)';
-      sections[1].style.transform = 'translateY(-100dvh)';
-    }, 100)
+    document.body.classList.add('slide-in');
   }
   const newUrl = new URL(window.location.href);
   newUrl.searchParams.delete('slide-in');
   window.history.replaceState(null, null, newUrl);
-});
