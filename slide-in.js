eae777f3efe6acb4d14afe13cd5d8d092115a884
gorilla-san <108207000+gorilla-san@users.noreply.github.com>
2024-05-02 08:22:33 +0200

    asd

--- slide-in.js
+++ slide-in.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const slideIn = urlParams.get('slide-in');
 
   if (slideIn === 'true') {
-    let sections = documet.querySelectorAll('.sections');
+    let sections = document.querySelectorAll('.sections');
     sections[0].style.transform = 'translateY(100dvh)';
     sections[1].style.transform = 'translateY(100dvh)';
     sections[0].style.transition = 'all 0.5s';
